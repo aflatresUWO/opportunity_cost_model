a0b0b1c15a55480f6874e7df19a7f1ada6a4abd4
aflatresUWO <82547969+aflatresUWO@users.noreply.github.com>
2022-06-14 15:12:45 -0400

    changes in plots

--- plots.py
+++ plots.py
@@ -15,12 +15,13 @@ n_db = int(np.sqrt(len(db)))
 db = db[0:n_db]
 
 p = np.array(data.loc[:,"P"])
-n_p = int(np.sqrt(len(p)))
-P = np.zeros(n_p)
-for i in range(0,int(np.sqrt(len(p)))):
-    P[i] = p[i*len(db)]
-print(P)
 h_data = np.array(data.loc[:,"h_x"])
+h_data = h_data[p>0]
+p=p[p>0]
+n_p = int(len(p)/len(db))
+P = np.zeros(n_p)
+for i in range(0,int(len(p)/len(db))):
+    P[i] = p[i*len(db)]
 
 h = np.zeros((n_p,n_db))
 k = 0
@@ -40,27 +41,65 @@ plt.ylabel("P")
 plt.title("Contour plot of ESS vs $\Delta b$ and probability of establishment")
 
 ###################################
+
+data = pd.read_csv('result_p_b2.csv')
+db = np.array(data.loc[:,"d_b"])
+n_db = int(np.sqrt(len(db)))
+db = db[0:n_db]
+
+p = np.array(data.loc[:,"P"])
+h_data = np.array(data.loc[:,"h_x"])
+h_data = h_data[p>0]
+p=p[p>0]
+n_p = int(len(p)/len(db))
+P = np.zeros(n_p)
+for i in range(0,int(len(p)/len(db))):
+    P[i] = p[i*len(db)]
+
+h = np.zeros((n_p,n_db))
+k = 0
+
+for i in range(0,n_p):
+    for j in range(0,len(db)):
+        h[i,j] = h_data[k]
+        k+=1
+fig, ax=plt.subplots()
+
+level = np.linspace(0,1,10)
+cp=ax.contourf(db,P,h,levels = level)
+
+fig.colorbar (cp)#Add a colorbar to a plot
+plt.xlabel('$\Delta b$')
+plt.ylabel("$s_u$")
+plt.title("Contour plot of ESS vs $\Delta b$ and survival of waiters")
+
+###################################
 data = pd.read_csv('result_p_s.csv')
 ds = np.array(data.loc[:,"d_s"])
 n_ds = int(np.sqrt(len(ds)))
 ds = ds[0:n_ds]
 
+
 p = np.array(data.loc[:,"P"])
-n_p = int(np.sqrt(len(p)))
+h_data = np.array(data.loc[:,"h_x"])
+h_data = h_data[p>0]
+p=p[p>0]
+n_p = int(len(p)/len(ds))
 P = np.zeros(n_p)
-for i in range(0,int(np.sqrt(len(p)))):
+for i in range(0,n_p):
     P[i] = p[i*len(ds)]
 
-h_data = np.array(data.loc[:,"h_x"])
-
-h = np.zeros((n_p,len(ds)))
+h = np.zeros((n_p,n_ds))
 k = 0
 
+
 for i in range(0,n_p):
-    for j in range(0,len(ds)):
+    for j in range(0,n_ds):
         h[i,j] = h_data[k]
         k+=1
+
 fig, ax=plt.subplots()
+
 
 level = np.linspace(0,1,10)
 cp=ax.contourf(ds,P,h,levels = level)
@@ -68,66 +107,41 @@ cp=ax.contourf(ds,P,h,levels = level)
 fig.colorbar (cp)#Add a colorbar to a plot
 plt.xlabel('$\Delta s$')
 plt.ylabel("P")
-#plt.title("Contour plot of ESS vs $\Delta b$ and probability of establishment")
-#----------
+plt.title("Contour plot of ESS vs $\Delta s$ and probability of establishment")
+
+##############################
+
 data = pd.read_csv('result_p_sj.csv')
 dsj = np.array(data.loc[:,"d_sj"])
-dsj = ds[0:10]
+n_dsj = int(np.sqrt(len(dsj)))
+dsj = dsj[0:n_dsj]
+
 
 p = np.array(data.loc[:,"P"])
-n_p = int(np.sqrt(len(p)))
-P = np.zeros(n_p)
-
-for i in range(0,int(np.sqrt(len(p)))):
-    P[i] = p[i*len(dsj)]
-
 h_data = np.array(data.loc[:,"h_x"])
-
-h = np.zeros((n_p,len(dsj)))
+h_data = h_data[p>0]
+p=p[p>0]
+n_p = int(len(p)/len(dsj))
+P = np.zeros(n_p)
+for i in range(0,n_p):
+    P[i] = p[i*len(dsj)]
+print(n_p)
+h = np.zeros((n_p,n_dsj))
 k = 0
 
+
 for i in range(0,n_p):
-    for j in range(0,len(dsj)):
+    for j in range(0,n_dsj):
         h[i,j] = h_data[k]
         k+=1
+
 fig, ax=plt.subplots()
 
+
 level = np.linspace(0,1,10)
-cp=ax.contourf(dsj,P,h)
+cp=ax.contourf(dsj,P,h,levels = level)
 
 fig.colorbar (cp)#Add a colorbar to a plot
 plt.xlabel('$\Delta s_j$')
 plt.ylabel("P")
-#plt.title("Contour plot of ESS vs $\Delta b$ and probability of establishment")
-#######################
-
-data = pd.read_csv('result_pj_sj.csv')
-dsj = np.array(data.loc[:,"d_sj"])
-dsj = ds[0:10]
-pj = np.array(data.loc[:,"Pj"])
-n_pj = int(np.sqrt(len(pj)))
-Pj = np.zeros(n_pj)
-
-for i in range(0,int(np.sqrt(len(pj)))):
-    Pj[i] = pj[i*len(dsj)]
-
-h_data = np.array(data.loc[:,"h_x"])
-
-h = np.zeros((n_pj,len(dsj)))
-k = 0
-
-for i in range(0,n_pj):
-    for j in range(0,len(dsj)):
-        h[i,j] = h_data[k]
-        k+=1
-fig, ax=plt.subplots()
-
-level = np.linspace(0,1,10)
-cp=ax.contourf(dsj,Pj,h)
-
-fig.colorbar (cp)#Add a colorbar to a plot
-plt.xlabel('$\Delta s_j$')
-plt.ylabel("p_j")
-#plt.title("Contour plot of ESS vs $\Delta b$ and probability of establishment")
-
-plt.show()
+plt.title("Contour plot of ESS vs $\Delta s_j$ and probability of establishment")
